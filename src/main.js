41792f6189509ecb0707a54731458286961e6ad5
CodeLump41 <114756610+CodeLump41@users.noreply.github.com>
2022-10-04 09:51:42 -0500

    More vue debugging

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.global.js";
 
-
-const app = createApp({
-    data(){
-        return{
-            message: "Hello World!"
-        }
+createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
     }
-});
-
-app.mount("#app");
+  }).mount('#app')
