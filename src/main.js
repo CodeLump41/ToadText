6ffa38f24482ee03834b282462903c19acfb033e
CodeLump41 <114756610+CodeLump41@users.noreply.github.com>
2022-10-04 11:08:58 -0500

    Vue comp testing

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import component from "./component.js"
 
-createApp({
-  data() {
-    return {
-      message: 'Hello Vue!'
-    }
-  }
-}).mount('#app')
+createApp(component).mount('#app')
